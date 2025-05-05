75351aa7d8bc64e1a75e9b9734c241e4dbb9245d
mjb90 <manasborah90@gmail.com>
2025-05-05 11:33:38 +0800

    MoodNft

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.18;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
-    uint256 private s_tokenCounter;
-    string private s_sadSvg;
-    string private s_happySvg;
+    error MoodNft_CantFlipMoodIfNotOwner();
 
-    constructor(string memory sadSvg, string memory happySvg) ERC721("Mood NFT", "MNFT") {
+    enum NFTState {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => NFTState) private s_tokenIdToState;
+    uint256 private s_tokenCounter;
+    string private s_sadSvgUri;
+    string private s_happySvgUri;
+
+    constructor(string memory sadSvgUri, string memory happySvgUri) ERC721("Mood NFT", "MNFT") {
         s_tokenCounter = 0;
-        sadSvg = sadSvg;
-        happySvg = happySvg;
+        s_sadSvgUri = sadSvgUri;
+        s_happySvgUri = happySvgUri;
     }
 
     function mintNft() public {
@@ -19,7 +28,43 @@ contract MoodNft is ERC721 {
         s_tokenCounter++;
     }
 
-    // function tokeURI(
-    //     uint256 tokenId
-    // ) public view override returns (string memory) {}
+    function flipMood(uint256 tokenId) public {
+        if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
+            revert MoodNft_CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToState[tokenId] == NFTState.HAPPY) {
+            s_tokenIdToState[tokenId] = NFTState.SAD;
+        } else {
+            s_tokenIdToState[tokenId] = NFTState.HAPPY;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory imageURI = s_happySvgUri;
+
+        if (s_tokenIdToState[tokenId] == NFTState.SAD) {
+            imageURI = s_sadSvgUri;
+        }
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
+    }
 }
---
+++ test/DeployMoodNft.t.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployMoodNft;
+
+    function setUp() external {
+        deployMoodNft = new DeployMoodNft();
+    }
+
+    function testSvgToImageUri() external view {
+        string memory svg = vm.readFile("images/happy.svg");
+        string memory expected =
+            "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEwMCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8+CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNzAiIGN5PSI4MiIgcj0iMTIiLz4KICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==";
+        string memory actual = deployMoodNft.svgToImageUri(svg);
+        console.log("Actual: ", actual);
+        console.log("Expected: ", expected);
+        assertEq(
+            keccak256(abi.encodePacked(actual)),
+            keccak256(abi.encodePacked(expected)),
+            "SVG to Image URI conversion failed"
+        );
+    }
+}
